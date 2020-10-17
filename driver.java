9448638db2367eb1dfe3a7ce856611841fd8bc68
sudhan.chitgopkar <sudhanc03@yahoo.com>
2020-10-16 22:01:21 -0400

    base driver class

---
+++ driver.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class driver {
+    public static void main (String [] args) {
+        //Set up user input
+        Scanner keyboard = new Scanner (System.in);
+
+        System.out.println(
+    } //main
+} //driver
